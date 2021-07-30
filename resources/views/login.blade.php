9c0e4a093b980ff7a3a1c1a820feac6b6661c7bb
adilsanak <adilsaputra0101@gmail.com>
2021-07-30 10:16:27 +0700

    solve url

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,11 +6,11 @@
     <title>Login | Site Administrator</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap Css -->
-    <link href="/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets') }}/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets') }}/css/icons.min.css" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets') }}/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
 </head>
 
 <body class="authentication-bg">
@@ -81,15 +81,15 @@
         <!-- end container -->
     </div>
     <!-- JAVASCRIPT -->
-    <script src="/assets/libs/jquery/jquery.min.js"></script>
-    <script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/libs/metismenu/metisMenu.min.js"></script>
-    <script src="/assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="/assets/libs/node-waves/waves.min.js"></script>
-    <script src="/assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
-    <script src="/assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/metismenu/metisMenu.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/simplebar/simplebar.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/node-waves/waves.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/jquery.counterup/jquery.counterup.min.js"></script>
     <!-- App js -->
-    <script src="/assets/js/app.js"></script>
+    <script src="{{ asset('assets') }}/js/app.js"></script>
 </body>
 
 </html>
--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -162,21 +162,22 @@
 
 @section('script')
     <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-    <script src="/assets/libs/jquery/jquery.min.js"></script>
-    <script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/libs/metismenu/metisMenu.min.js"></script>
-    <script src="/assets/libs/simplebar/simplebar.min.js"></script>
-    <script src="/assets/libs/node-waves/waves.min.js"></script>
-    <script src="/assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
-    <script src="/assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/jquery/jquery.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/metismenu/metisMenu.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/simplebar/simplebar.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/node-waves/waves.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/jquery.counterup/jquery.counterup.min.js"></script>
     <!-- Datatable -->
-    <script src="/assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
     <!-- Responsive examples -->
-    <script src="/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="{{ asset('assets') }}/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js">
+    </script>
     <!-- Datatable init js -->
-    <script src="assets/js/pages/datatables.init.js"></script>
+    <script src="{{ asset('assets') }}/js/pages/datatables.init.js"></script>
     <!-- App js -->
-    <script src="/assets/js/app.js"></script>
+    <script src="{{ asset('assets') }}/js/app.js"></script>
 @endsection
